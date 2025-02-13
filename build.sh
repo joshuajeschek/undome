df21399a6c4ecb647b26c265994a79bc7ab855a5
Joshua Jeschek <dev@jeschek.eu>
2025-02-13 01:46:03 +0100

    feat(build): manually settable central device

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
+BUILD_FLAGS="-DSHIELD=splitkb_aurora_lily58_$1"
+if [ -n "$CENTRAL" ]; then
+  BUILD_FLAGS="$BUILD_FLAGS -DCONFIG_ZMK_SPLIT_ROLE_CENTRAL=y"
+fi
+
 build_side() {
-  ./exec.sh west build -d build/$1 -b nice_nano_v2 -- -DSHIELD="splitkb_aurora_lily58_$1"
+  ./exec.sh west build -d build/$1 -b nice_nano_v2 -- $BUILD_FLAGS
   docker cp "$(cat .devcontainer-id):/workspaces/zmk/app/build/$1/zephyr/zmk.uf2" undome-$1.uf2
 }
 
